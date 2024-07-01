5c9888fa0952cc7688d69a257e5e989e3ae25ee9
Davide Bonetto <bonetto.davide98@gmail.com>
2024-07-01 11:54:42 +0200

    Added background color and first page header

--- libreria/src/components/body/aboutMe/AboutMe.js
+++ libreria/src/components/body/aboutMe/AboutMe.js
@@ -1,9 +1,19 @@
 import './AboutMe.css';
+import photo from "../../../assets/photo.jpg";
 
 export default function AboutMe() {
     return (
-        <div className="scrollable-wrapper">
-            
+        <div className="scrollable-wrapper upper-gradient">
+            <div className="half_screen">
+                <div className="centered-text">
+                    <p>Hello!</p>
+                    <h1>My name is Davide</h1>
+                    <h2>I'm a full-stack developer and computer vision specialist</h2> 
+                </div>
+            </div>
+            <div className="half_screen">
+                <img src={photo}></img>
+            </div>
         </div>
     );
 }
